b1d1b3fe9b77594b16b32559faf10e71a60f0345
novakvova <novakvova@gmail.com>
2018-07-11 15:29:26 +0300

    Begin

---
+++ backend/Controllers/QuestionsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+        private readonly QuizeContext _context;
+        public QuestionsController(QuizeContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return new Models.Question[]
+            {
+                new Models.Question { Text = "Собака" },
+                new Models.Question { Text = "Дичка" }
+            };
+        }
+        //POST api/questions
+        [HttpPost]
+        public void Post([FromBody]Models.Question question)
+        {
+            _context.Questions.
+                Add(question);
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ backend/QuizeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    public class QuizeContext : DbContext
+    {
+        public QuizeContext(DbContextOptions<QuizeContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Models.Question> Questions { get; set; }
+    }
+}
